b0a338710032c7e2135ce1796a21a0840c525b71
muhammadashhad1266 <2501472@students.au.edu.pk>
2026-06-25 23:23:36 +0500

    Created Ui rules within the main_window. initiallized basic view

--- gui/main_window.py
+++ gui/main_window.py
@@ -7,6 +7,9 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QVBoxLayout,
     QMainWindow,
+    QWidget,
+    QHBoxLayout,
+    QStackedWidget,
 )
 from PySide6.QtCore import Slot, QObject, Signal
 from PySide6.QtGui import QIcon
@@ -23,7 +26,10 @@ from PySide6.QtGui import QIcon
     QDialog: Dialog Class
     QLineEdit: Line Edit Class
     QVBoxLayout: Vertical Layout Class
+    QHBoxLayout: Horizontal Layout Class
+    QWidget: Widget Class
     QMainWindow: Main Window Class
+    QStackedWidget: Stacked Widget Class
     Slot: Slot Class
     QObject: Object Class
     Signal: Signal Class
@@ -39,9 +45,31 @@ from PySide6.QtGui import QIcon
 class mainWin(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # title and icon
         self.setWindowTitle("SigmaMath")
         self.setWindowIcon(QIcon("icon\sigma.png"))
+
+        # base size when opened
         self.setMinimumSize(450, 600)
+
+        # creating custom interface using blank canvas (QWidget)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        # creating master layout
+        self.masterLayout = QHBoxLayout(self.centralWidget)
+        self.masterLayout.setContentsMargins(0, 0, 0, 0)
+        self.masterLayout.setSpacing(0)
+
+        # creating sidebar
+        self.sidebarWidget = QWidget()
+        self.sidebar_layout = QVBoxLayout(self.sidebarWidget)
+        
+
+        # creating view stack
+        self.view_stack = QStackedWidget()
+        self.masterLayout.addWidget(self.view_stack, stretch=1)
 
     """ 
     Reads the external QSS file and applies it globally to the window.
--- gui/views/basic.py
+++ gui/views/basic.py
@@ -1,2 +1,16 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLineEdit
 from gui.widgets import button
+
+""" Imported libraries
+    PySide6: PySide6 Library
+    custom widgets {file location : gui/widgets.py} contains custom widgets class
+"""
+
+""" Imported classes from PySide6
+    QWidget: Widget Class
+    QVBoxLayout: Vertical Layout Class
+    QGridLayout: Grid Layout Class
+    QLineEdit: Line Edit Class
+"""
+
 
